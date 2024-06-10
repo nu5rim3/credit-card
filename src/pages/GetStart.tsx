a4e5e1c2044c7823931f228e4d00362ce4faa337
Nusri Samath <nusrisamath@gmail.com>
2024-06-10 10:49:52 +0530

    update: change the button position

--- src/pages/GetStart.tsx
+++ src/pages/GetStart.tsx
@@ -12,16 +12,13 @@ const GetStart = () => {
     }
     return (
         <div className="bg-[url('/img/hero-pattern.svg')] flex justify-center items-center min-h-screen">
-            <div className='bg-primary-100/60 p-8 rounded-lg shadow-md w-full my-2 animate-fade-up animate-duration-[1000ms] animate-once flex flex-col justify-between items-center h-screen'>
+            <div className='bg-primary-100/60 p-8 rounded-lg shadow-md w-full my-2 animate-fade-up animate-duration-[1000ms] animate-once flex flex-col justify-between items-center h-screen pb-20'>
                 <img className="w-36 sm:w-52 animate-fade-up animate-duration-[500ms] animate-once" src={logo} />
                 <div className="text-3xl sm:text-5xl mb-2 text-primary-900 animate-fade-up animate-duration-[2000ms] animate-once text-center sm:text-left">LOLC FINANCE CREDIT CARDS</div>
                 <img src={cardImage} className='animate-fade-up animate-duration-[3000ms] animate-once w-full sm:w-1/2' />
                 <div className="text-3xl sm:text-5xl font-extrabold mb-2 text-primary-900 animate-fade-up animate-duration-[2000ms] animate-once text-center sm:text-left">Fuel the Goodness in You.</div>
                 <Button variant={'primary'} className={'w-full m-1 sm:w-44'} onClick={onGetStartClick}>Get Start</Button>
-
             </div>
-
-
         </div>
     )
 }
