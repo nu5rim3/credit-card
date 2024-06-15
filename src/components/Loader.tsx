2622462da294cd422e070bd385c94c201ace0a05
nu5rim3 <nusrisamath@gmail.com>
2024-06-15 11:55:07 +0530

    updated: lading

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ import logo from '../assets/images/lolcf_logo.svg'
 
 const Loader = () => {
     return (
-        <div className='w-screen h-screen bg-primary-50 flex justify-center items-center'>
+        <div className='w-screen h-screen backdrop-blur-sm bg-primary-50/70  flex justify-center items-center'>
             <img className="w-20 mb-10 animate-ping" src={logo} />
         </div>
     )
