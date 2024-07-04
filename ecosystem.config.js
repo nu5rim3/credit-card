c732feada0edfe7275f3a08fb054e47f0d578b8b
nu5rim3 <nusrisamath@gmail.com>
2024-07-04 18:10:03 +0530

    updated:

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   apps: [
     {
-      name: "credit-card-inquiries-web",
+      name: "credit-card-web",
       script: "./server.js",
       env_dev: {
         NODE_ENV: "development",
