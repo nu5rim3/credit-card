8745fef7b6b9f44635ac41057eade6e2074172b3
nu5rim3 <nusrisamath@gmail.com>
2024-07-04 17:56:49 +0530

    updated: new config

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+npm run build
+
+pm2 restart ecosystem.config.js --env dev --update-env
---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  apps: [
+    {
+      name: "credit-card-inquiries-web",
+      script: "./server.js",
+      env_dev: {
+        NODE_ENV: "development",
+      },
+      env_prod: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
